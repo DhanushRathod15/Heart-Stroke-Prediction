eacf194d52eceddf47f679c5f7288af47dd438e8
Dhanush Rathod <rathoddhanush15@gmail.com>
2024-08-02 00:29:44 +0530

    "Added AWS S3 storage functionality, model pusher component, and S3 estimator entity"

---
+++ demo.py
@@ -0,0 +1,3 @@
+from stroke.pipelin.traing_pipeline import TrainPipeline
+train_pipeline = TrainPipeline()
+train_pipeline.run_pipeline()
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="heart-stroke-prediction",
+    version="0.1.1",
+    author="Dhanush",
+    author_email="rathoddhanush15@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
